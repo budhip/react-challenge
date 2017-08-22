9c71a5fe6702c0c2ce147a0a19661ee110453415
budhip <ryo.bunraku@yahoo.com>
2017-08-22 16:02:10 +0700

    Release 2 - mendapatkan data dari api dan menampilkannya

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import ContentCuaca from './components/ContentCuaca';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <Header />
+        <div className="container ">
+          <div className="text-center">
+            <label className="label">Nama Kota</label>
+            <p className="control">
+              <input className="input" type="text" placeholder="--Nama Kota--" />
+            </p>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <hr />
+        <ContentCuaca />
       </div>
     );
   }
